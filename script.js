655b9eb4cb6565bef17de1e9be0beb84aae9edce
RobertoFloris <florisroberto99@gmail.com>
2024-11-13 15:20:55 +0100

    [add] if in script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+// const km = 150;
+// const age = 70;
+
 const km = parseInt(prompt("Inserire quanti km si dovranno percorrere"));
 console.log("Si percorranno: " + km + "Km");
 
 const age = parseInt(prompt("Inserire la tua età"));
 console.log("Hai: " + age + " anni");
+
+let price = km * 0.21;
+
+if (age<18){
+  price*=0.8; //20% di sconto
+  console.log("Sei minorenne per cui ti viene applicato uno sconto del 20%");
+} else if(age>=65) {
+  price*=0.6; //40% di sconto
+  console.log("Sei over65 per cui ti viene applicato uno sconto del 40%");
+}
+
+console.log("Il prezzo del tuo biglietto è: "+ price + "€"); 
