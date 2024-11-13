4e3623a02fa8512cb93a981a5514a15b35d56182
RobertoFloris <florisroberto99@gmail.com>
2024-11-13 14:57:08 +0100

    [add] variables km and age in script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const km = parseInt(prompt("Inserire quanti km si dovranno percorrere"));
+console.log("Si percorranno: " + km + "Km");
+
+const age = parseInt(prompt("Inserire la tua età"));
+console.log("Hai: " + age + " anni");
